81cf27af794be5b35c6d578ebfe1485f13e2d3a2
wushaoxiong88888 <1120450191@qq.com>
2017-12-28 09:42:01 +0800

    commit xiangmu

--- MyKotlin/app/src/main/java/com/pc/mykotlin/powerFX/ListActivity.kt
+++ MyKotlin/app/src/main/java/com/pc/mykotlin/powerFX/ListActivity.kt
@@ -1,9 +1,11 @@
 package com.pc.mykotlin.powerFX
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import com.pc.mykotlin.JiaoZi.JiaoZiActivity
 import com.pc.mykotlin.R
 import com.pc.mykotlin.powerFX.adapter.ListRlvAdapter
 import com.pc.mykotlin.powerFX.bean.ListBean
@@ -38,10 +40,25 @@ class ListActivity : AppCompatActivity() ,ListInfoView {
     //实现数据接口
     override fun listShow(listBean: ListBean) {
         val itemList = listBean.itemList
+
         //设置rlv数据
         list_rlv.layoutManager = LinearLayoutManager(this)
         var adapter = ListRlvAdapter(this,itemList!!)
         list_rlv.adapter = adapter
+
+        //设置点击跳转
+        adapter.setOnItemListListener(object : ListRlvAdapter.OnItemListListener{
+            override fun OnItemListClick(position: Int) {
+                //获取传的值
+                val category = listBean.itemList?.get(position)?.data?.category
+                //Intent跳转传值
+                var i: Intent = Intent(this@ListActivity,JiaoZiActivity::class.java)
+                i.putExtra("category",category)
+                i.putExtra("position",position)
+                startActivity(i)
+            }
+
+        })
     }
 
     /*override fun getPresenter(): ListPresenter? {
--- MyKotlin/app/src/main/java/com/pc/mykotlin/powerFX/adapter/ListRlvAdapter.kt
+++ MyKotlin/app/src/main/java/com/pc/mykotlin/powerFX/adapter/ListRlvAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.pc.mykotlin.R
@@ -18,11 +19,28 @@ class ListRlvAdapter(context: Context,list: List<ListBean.ItemListBean>): Recycl
     private var context:Context = context
     private var list:List<ListBean.ItemListBean> = list
 
+    //接口回调
+    var listListener:OnItemListListener? = null
+    interface OnItemListListener{
+        fun OnItemListClick(position: Int)
+    }
+    fun setOnItemListListener(listListener:OnItemListListener){
+        this.listListener = listListener
+    }
+
     override fun onBindViewHolder(holder: MyViewHolder?, position: Int) {
 
         //设置图片
         Glide.with(context).load(list.get(position).data?.cover?.feed).into(holder?.iv)
         holder?.tv?.text = list.get(position).data?.title
+
+        //设置点击跳转
+        holder?.rl?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                listListener?.OnItemListClick(position)
+            }
+
+        })
 
     }
 
@@ -39,5 +57,6 @@ class ListRlvAdapter(context: Context,list: List<ListBean.ItemListBean>): Recycl
     class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
         var iv:ImageView = itemView?.findViewById(R.id.list_rlv_iv) as ImageView
         var tv:TextView = itemView?.findViewById(R.id.list_rlv_tv_title) as TextView
+        var rl:RelativeLayout = itemView?.findViewById(R.id.list_rlv_rl) as RelativeLayout
     }
 }
