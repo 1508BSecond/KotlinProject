c3bf26b4441caf563b7788b8c09b3bc050427513
wushaoxiong88888 <1120450191@qq.com>
2017-12-28 19:21:14 +0800

    commit xiangmu

---
+++ MyKotlin/app/src/main/java/com/pc/mykotlin/publicUI/WelcomeActivity.kt
@@ -0,0 +1,45 @@
+package com.pc.mykotlin.publicUI
+
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.pc.mykotlin.R
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        //给图片设置动画
+        var animatorX: ObjectAnimator = ObjectAnimator.ofFloat(wel_iv,"scaleX",0.1f,1.5f)
+        var animatorY: ObjectAnimator = ObjectAnimator.ofFloat(wel_iv,"scaleY",0.1f,1.5f)
+        var animSet: AnimatorSet = AnimatorSet()
+        animSet.play(animatorX).with(animatorY)
+        animSet.setDuration(3000)
+        animSet.start()
+
+        //动画监听器
+        animSet.addListener(object :Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                var i: Intent = Intent(this@WelcomeActivity,MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+        })
+    }
+}
